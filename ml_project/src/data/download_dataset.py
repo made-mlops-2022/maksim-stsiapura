e3c77a2b8c3827683e59b5c4155f5423f7653233
maxima1ist <mksstepura@gmail.com>
2022-10-29 20:37:24 +0300

    Use kaggle credentials in CI

--- ml_project/src/data/download_dataset.py
+++ ml_project/src/data/download_dataset.py
@@ -1,3 +1,4 @@
+from src.logger.base_logger import logger
 import os
 import json
 
@@ -5,12 +6,15 @@ DATASET_URL = "https://www.kaggle.com/datasets/cherngs/heart-disease-cleveland-u
 DATA_PATH = "data/"
 KAGGLE_CREDS_PATH = "credentials/kaggle.json"
 
+try:
+    with open(KAGGLE_CREDS_PATH) as fin:
+        kaggle_creds = json.loads(fin.read())
+    os.environ["KAGGLE_USERNAME"] = kaggle_creds["username"]
+    os.environ["KAGGLE_KEY"] = kaggle_creds["key"]
+    from kaggle.api.kaggle_api_extended import KaggleApi
+except FileNotFoundError as err:
+    logger.error("Credetials not provided: %s", err)
 
-with open(KAGGLE_CREDS_PATH) as fin:
-    kaggle_creds = json.loads(fin.read())
-os.environ["KAGGLE_USERNAME"] = kaggle_creds["username"]
-os.environ["KAGGLE_KEY"] = kaggle_creds["key"]
-from kaggle.api.kaggle_api_extended import KaggleApi
 
 api = KaggleApi()
 api.authenticate()
